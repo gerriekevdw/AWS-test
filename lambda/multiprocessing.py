65add40ac0cd8ecbf38b2133d8a3efcac3e610b0
Gerrieke <gerrieke@ip-172-31-34-104.eu-central-1.compute.internal>
2022-05-23 14:48:15 +0000

    First complete version

--- lambda/multiprocessing.py
+++ lambda/multiprocessing.py
@@ -1,83 +1,86 @@
+from concurrent.futures import process
+import gc
+import sys
+import numpy as np
+import pandas as pd
 from multiprocessing import Pipe
 from multiprocessing import Process
 from multiprocessing.connection import _ConnectionBase
 
+def values_to_df(value, batch_nr, value_nr, connection:_ConnectionBase):
+    print(f'This is batch nr {batch_nr} - value nr {value_nr}:  {value}')
+    
+    try:
+        df = pd.DataFrame({'batch': batch_nr, 'value': value, 'value_nr': value_nr}, index=[0])
+        connection.send(df)
+        connection.close()
+        return
+    except:
+        err = sys.exc_info()[0]
+        print(f"Exception {batch_nr}: {value_nr} - {err}")
+        connection.send(None)
+        connection.close()
+        return 
+        
+def process_batch(batch, batch_nr):
+    results = []
+    processes = []
+    connections = []
+
+    value_nr = 0
+    for value in batch:
+        parent_conn, child_conn = Pipe()
+        connections.append(parent_conn)
+
+        process = Process(target=values_to_df, args=(value, batch_nr, value_nr, child_conn))
+        processes.append(process)
+
+    for process in processes:
+        process.start()
+
+    for conn in connections:
+        try:
+            df = conn.recv()
+            if df is not None:
+                results.append(df)
+
+            conn.close()
+
+        except EOFError:
+            conn.close()
+            pass
+
+    for process in processes:
+        process.join()
+
+    return results
+
+
 def lambda_handler(event, context):
+    # Get number of parallel processes to run, default 25
+    batch_size = event.get('batch', 25)
+    nr_total = event.get('total', 150)
+    
+    values = np.random.rand(nr_total)
+    
+    results = []
+    batch_nr = 0
+    for i in range(0, len(values), batch_size):
+        values_batch = values[i: i + batch_size]
+        sub_results = process_batch(values_batch, batch_nr)
+        results = results + sub_results
+        gc.collect()
+        batch_nr += 1
+    
 
-        # results: List[pd.DataFrame] = []
-        # paths: List[str] = []
-        # prefix_objs = self.s3_resource.objects.filter(Prefix=prefix)
-
-        # for obj in prefix_objs:
-        #     paths.append(obj.key)
-
-        # if len(prefix_objs) == 0:
-        #     print(f"No objects found for {prefix}")
-
-        # # Split in batches
-        # for i in range(0, len(paths), batchsize_json_files):
-        #     paths_batch = paths[i : i + batchsize_json_files]
-        #     sub_results = self.read_json_files_to_df(paths_batch)
-        #     results = results + sub_results
-        #     gc.collect()
-
-        # logging.log(
-        #     logging.INFO, f"len raw data frames: {len(results)} for prefix {prefix}"
-        # )
-
-        # if len(results) > 0:
-        #     return pd.concat(results, sort=False).reset_index(drop=True)
-
-        # return None
-
+        if len(results)>0:
+            final_result = pd.concat(results, ignore_index=True)
+            print(f"Shape final result: {final_result.shape}")
+            print(final_result.groupby('batch')['value_nr'].agg(['size', 'min', 'max']).to_string())
         
-        # results = []
-        # processes = []
-        # connections = []
+        else:
+            final_result = None
+            print("No final result")
 
-        # for path in paths:
-        #     parent_conn, child_conn = Pipe()
-        #     connections.append(parent_conn)
-
-        #     process = Process(target=self.read_json_to_df, args=(path, child_conn))
-        #     processes.append(process)
-
-        # for process in processes:
-        #     process.start()
-
-        # for conn in connections:
-        #     try:
-        #         df = conn.recv()
-        #         if df is not None:
-        #             results.append(df)
-
-        #         conn.close()
-
-        #     except EOFError:
-        #         conn.close()
-        #         pass
-
-        # for process in processes:
-        #     process.join()
-
-        # return results
-
-
-        
-        # try:
-        #     obj = self.s3_client.get_object(Bucket=self.bucket_name, Key=key)
-        #     df = pd.read_json(obj["Body"], convert_dates=False)
-        #     conn.send(df)
-        #     conn.close()
-        #     return
-        # except ClientError:
-        #     time.sleep(backoff)
-        #     self.read_json_to_df(key, conn, backoff * 2)
-        # except:  # noqa E722 ## Ignore use of bare 'except' for Flake8
-        #     err = sys.exc_info()[0]
-        #     logging.exception(f"Exception reading {key}: {err}")
-        #     conn.send(None)
-        #     conn.close()
-        #     return
     return True
 
