0a10553133c37274a577f49e5ea067833123be5f
Gerrieke Druijff-van de Woestijne <gerriekevdw@gmail.com>
2022-05-24 10:44:30 +0200

    Possible to push to ECR

--- lambda/build_lambda.sh
+++ lambda/build_lambda.sh
@@ -1,8 +1,8 @@
-aws ecr get-login-password | docker login --username AWS --password-stdin 254701324686.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password --region eu-central-1| docker login --username AWS --password-stdin 254701324686.dkr.ecr.eu-central-1.amazonaws.com
 docker build -t data-pipeline -f DockerfileAWSLambda .
 docker tag data-pipeline 254701324686.dkr.ecr.eu-central-1.amazonaws.com/test-lambda
 docker push 254701324686.dkr.ecr.eu-central-1.amazonaws.com/test-lambda
 
 
 # update lambda functions
-# aws lambda update-function-code --function-name test-lambda --image-uri 254701324686.dkr.ecr.eu-central-1.amazonaws.com/test-lambda:latest
+aws lambda update-function-code --function-name test-lambda --image-uri 254701324686.dkr.ecr.eu-central-1.amazonaws.com/test-lambda:latest
